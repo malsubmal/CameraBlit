1ab9b13135434ab4189fc50da366dd6be1750b4e
malsubmal <63635575+malsubmal@users.noreply.github.com>
2026-01-20 19:08:25 +0700

    Add UI

---
+++ Assets/Scripts/SimpleUIHandle/JustCapturePhotoHost.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using GameFlow;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using ZeroMessenger;
+
+namespace SimpleUIHandle
+{
+    public class JustCapturePhotoHost : MonoBehaviour
+    {
+        [SerializeField] private GameObject _hostObject;
+        [SerializeField] private RawImage _photoImage;
+        [SerializeField] private TMP_Text _tagLine;
+        [SerializeField] private int _autoOffCanvas = 3;
+
+        private const string Match = "It's a Match! You Win";
+        private const string NoMatch = "It's Not a Match";
+        
+        private IDisposable _disposable;
+        private void Awake()
+        {
+            _disposable = MessageBroker<PhotoMatchStatus>.Default.Subscribe(OnPhotoCapture);
+        }
+
+        private void OnDestroy()
+        {
+            _disposable?.Dispose();
+        }
+
+        private async void OnPhotoCapture(PhotoMatchStatus payload)
+        {
+            var texture = payload.photoTexture;
+            var isMatch = payload.IsMatch();
+            
+            _photoImage.texture = texture;
+            _tagLine.text = isMatch ? Match : NoMatch;
+            
+            _hostObject.SetActive(true);
+
+            if (!isMatch)
+            {
+                await UniTask.WaitForSeconds(_autoOffCanvas);
+                
+                if (_hostObject is not null)
+                    _hostObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SimpleUIHandle/LevelPhotoHost.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using GameFlow;
+using Level;
+using UnityEngine;
+using UnityEngine.UI;
+using Utils;
+using ZeroMessenger;
+
+namespace SimpleUIHandle
+{
+    public class LevelPhotoHost : MonoBehaviour
+    {
+        [SerializeField] private Image _levelPhoto;
+        [SerializeField] private PhotoAlbum.PhotoAlbum _photoAlbum;
+
+        private IDisposable _disposable;
+        private void Awake()
+        {
+            _disposable = MessageBroker<FinishInitialization>.Default.Subscribe(OnReceiveLevelInit);
+        }
+
+        private void OnDestroy()
+        {
+            _disposable?.Dispose();
+        }
+
+        private void OnReceiveLevelInit(FinishInitialization payload)
+            => InitLevelPhotoUI(payload.LevelConfig);
+
+        public async void InitLevelPhotoUI(LevelConfig levelConfig)
+        {
+            var levelPhotos = levelConfig.LevelPhoto;
+
+            if (levelPhotos.IsNullOrEmpty())
+            {
+                Debug.LogError("Level Photo IsNullOrEmpty");
+                return;
+            }
+
+            var levelPhotoData = levelPhotos[0];
+
+            var spr = await _photoAlbum.LoadPhotoAsSprite(levelPhotoData.PhotoId);
+            
+            _levelPhoto.sprite = spr;
+        }
+    }
+}
